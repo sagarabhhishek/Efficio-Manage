45150e63c6c6d790dfdad3a17d1a2def5ad9bf27
Abhishek Sagar <abhisheksagar300500@gmail.com>
2025-04-13 15:49:52 +0530

    sameSite to None and credentials include in frontend

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -5,6 +5,7 @@ const API_URI = import.meta.env.VITE_APP_BASE_URL;
 
 const baseQuery = fetchBaseQuery({
   baseUrl: API_URI + "/api",
+  credentials: "include",
 });
 
 export const apiSlice = createApi({
